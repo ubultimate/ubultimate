93af7dd2568dea44908483b8f1ef26d5ccc75378
ubultimate <buffaloultimate@gmail.com>
2022-11-17 17:14:44 -0500

    Changes JSON file and adds image to js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 // document.getElementById('btn').addEventListener('click', event => {clickFunc()});
 
-async function getPlayerData() {
+async function playerData() {
     const requestURL = 'https://ubultimate.github.io/ubultimate/test.json';
     const request = new Request(requestURL);
     const response = await fetch(request);
@@ -14,20 +14,37 @@ async function getPlayerData() {
         let currentPlayerNum = currentPlayer['Number']
         let currentPlayerImg = currentPlayer['imgURL']
         let currentPlayerFlag = currentPlayer['Redflag']
-        console.log(currentPlayer)
 
-        populatePlayers(currentPlayerName, currentPlayerLoc, currentPlayerPos, currentPlayerNum, currentPlayerImg, currentPlayerFlag)
-
+        populatePlayers(currentPlayerName, currentPlayerLoc, currentPlayerPos, currentPlayerNum, currentPlayerImg, currentPlayerFlag);
     }
-
 } // End Function - getData 
 
 
 function populatePlayers(inputName, inputHome, inputPos, inputNum, inputImg, inputFlag){
-    const repeatText = "<div id='div1' class='u-align-center u-container-style u-list-item u-radius-20 u-repeater-item u-shape-round u-white u-list-item-1'> <div id='div2' class='u-container-layout u-similar-container u-container-layout-1'> <img id='img1' class='u-expanded-width u-image u-image-round u-radius-20 u-image-1' data-image-width='598' data-image-height='598' src=" + "" + "/> <p id='p1' class='u-align-left u-text u-text-grey-30 u-text-3'>"+ inputHome +"</p><h3 id='p2' class='u-align-left u-text u-text-default u-text-palette-2-base u-text-4'>" + inputName + "</h3> <p id='p3' class='u-align-left u-text u-text-body-color u-text-5'>#" + inputNum +"</p> <p id='p4' class='u-align-left u-text u-text-body-color u-text-5'>📍:" + inputPos + "</p> <p id='p5' class='u-align-left u-text u-text-body-color u-text-5'>🚩:" + inputFlag + "</p></div></div>"
+    
+    const repeatText = "<div id='div1' class='u-align-center u-container-style u-list-item u-radius-20 u-repeater-item u-shape-round u-white u-list-item-1'> <div id='div2' class='u-container-layout u-similar-container u-container-layout-1'> <img id='img1' class='u-expanded-width u-image u-image-round u-radius-20 u-image-1' data-image-width='598' data-image-height='598'/> <p id='p1' class='u-align-left u-text u-text-grey-30 u-text-3'>"+ inputHome +"</p><h3 id='p2' class='u-align-left u-text u-text-default u-text-palette-2-base u-text-4'>" + inputName + "</h3> <p id='p3' class='u-align-left u-text u-text-body-color u-text-5'>#" + inputNum +"</p> <p id='p4' class='u-align-left u-text u-text-body-color u-text-5'>📍:" + inputPos + "</p> <p id='p5' class='u-align-left u-text u-text-body-color u-text-5'>🚩:" + inputFlag + "</p></div></div>"
     document.getElementById('enterHere').outerHTML += repeatText;
+    document.getElementById('img1').src = inputImg;
 } // End function - test
 
 
 
-getPlayerData();
+playerData();
+
+
+function stuff(){
+    const imageUrl = "https://resizing.flixster.com/KZNkDNclEpm3XM2Aket8wTeVd9g=/300x300/v2/https://flxt.tmsimg.com/assets/41469_bu.jpg";
+
+    
+    // var val = document.getElementById('imagename').value,
+    //     src = 'http://webpage.com/images/' + val +'.png',
+    //     img = document.createElement('img');
+
+    // img.src = src;
+    // document.body.appendChild(img);
+    // }
+
+
+}
+
+
